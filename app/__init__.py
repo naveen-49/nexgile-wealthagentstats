9c3097645764522d3bf7ec014743b91aaf6e4e3d
naveen-49 <naveenyadavmettela@gmail.com>
2026-09-18 16:38:35 +0530

    Fix CORS for React frontend

--- app/__init__.py
+++ app/__init__.py
@@ -24,11 +24,13 @@ def create_app():
         resources={
             r"/api/*": {
                 "origins": [
-                    "http://localhost:5173",
-                    "http://127.0.0.1:5173",
-                    "http://localhost:5500",
-                    "http://127.0.0.1:5500"
-                ]
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
+]
             }
         },
         allow_headers=[
