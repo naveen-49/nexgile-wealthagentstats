58a37b038259610bab7e4b47b6d06260cf4fea66
naveen-49 <naveenyadavmettela@gmail.com>
2026-09-22 13:50:51 +0530

    Fix Flask-Migrate initialization

--- app/__init__.py
+++ app/__init__.py
@@ -53,7 +53,7 @@ def create_app():
     # =====================================================
 
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
     jwt.init_app(app)
 
     # =====================================================
